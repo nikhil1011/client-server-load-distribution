17673622f706f9d8aea05f6f6436849a59693ac6
Nikhil Vandanapu <nikhilvan10@gmail.com>
2018-03-06 13:01:13 -0600

    Client program

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+import sys
+import random
+
+class Client:
+    def __init__(self, port_no):
+        self.port = port_no
+
+    def connect(self):
+
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = ('localhost', self.port)
+        print('connecting to %s port %s' % server_address)
+        sock.connect(server_address)
+
+        message = "This is a message."
+            # Look for the response
+        amount_received = 0
+        amount_expected = len(message.encode('utf-8'))
+    
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print('received "%s"' % data.decode('utf-8'))
+
+#my_server = server.Server(1,10000)
+#my_server.launch()
+my_client = Client(10000)
+my_client.connect()
--- server.py
+++ server.py
@@ -1,10 +1,36 @@
 import socket
 import sys
+import random
 
-#server socket setup and binding
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+class Server:
 
-no_of_tasks = sys.argv[1]
+    def __init__(self, no_of_tasks, port_no):
+        self.port = port_no
+        self.tasks = []
+        for i in range(1, no_of_tasks + 1):
+            self.tasks.append(i)
+
+    def launch(self):
+        #server socket setup and binding
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('localhost', self.port)
+        print (sys.stderr, 'starting up on %s port %s' % server_address)
+        sock.bind(server_address)
+
+        sock.listen(1)
+
+        while(len(self.tasks) > 0 ):
+            print("waiting for a client to accept load")
+            connection, client_address = sock.accept()
+            try:
+                print("connection from: ", client_address)
+                next_i= random.randint(0,len(self.tasks) - 1)
+                message = "This is a message."
+                connection.sendall(message.encode('utf-8'))
+                self.tasks.pop(next_i)
+            finally:
+                connection.close()
+        sock.close()
+
+my_server = Server(1,10000)
+my_server.launch()
